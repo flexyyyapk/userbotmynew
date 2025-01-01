24e65be8ff01a940983734352b7ed0b32276e04b
flexyyyapk <147950098+flexyyyapk@users.noreply.github.com>
2025-01-01 17:58:29 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 __name__ = 'NewUserBotV2'
-__version__ = '0.0.2'
+__version__ = '0.0.1'
 __author__ = 'flexyyyapk'
 __link__ = 'https://t.me/d0br1u'
 __gitHub__ = 'https://github.com/flexyyyapk/userbotmynew'
 __news__ = '<blockquote>✨Обновление 0.0.2</blockquote>\n<blockquote>💡Что нового?</blockquote>\n<blockquote>- Установка плагина, удаления плагина🎁\n- Обновления скрипта🎃</blockquote>'
+__python__ = '>=3.8'
+__modules__ = ['pyrogram', 'terminaltexteffects', 'requests']
