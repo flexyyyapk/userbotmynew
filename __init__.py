e26c016c3f843866578f7f229433f9305afd6e75
flexyyy <147950098+flexyyyapk@users.noreply.github.com>
2025-03-18 23:29:06 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ __version__ = '0.0.6'
 __author__ = 'flexyyyapk'
 __link__ = 'https://t.me/oprosmenya'
 __gitHub__ = 'https://github.com/flexyyyapk/userbotmynew'
-__news__ = '<blockquote>✨Обновление 0.0.6</blockquote>\n<blockquote>💡Что нового?</blockquote>\n<blockquote> - 💱Добавлена команда {., !, /}excrypto которая показывает курс крипты.Подробнее в плагине StartedPack\n - Теперь у проекта есть нормальное название: ModuGram</blockquote>'
+__news__ = '<blockquote>✨Обновление 0.0.6</blockquote>\n<blockquote>💡Что нового?</blockquote>\n<blockquote> - 💱Добавлена команда {., !, /}excrypto которая показывает курс крипты.Подробнее в плагине StartedPack\n - Теперь у проекта есть нормальное название: ModuFlex</blockquote>'
 __python__ = '>=3.8'
 __modules__ = ['pyrogram', 'terminaltexteffects', 'requests', 'platform', 'ccxt', 'pyfiglet']
