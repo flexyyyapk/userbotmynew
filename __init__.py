a350fd3e3a64c2bfdbe0e62fe594486668d7844d
flexyyy <147950098+flexyyyapk@users.noreply.github.com>
2025-03-19 22:39:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ __version__ = '0.0.7b1'
 __author__ = 'flexyyyapk'
 __link__ = 'https://t.me/oprosmenya'
 __gitHub__ = 'https://github.com/flexyyyapk/userbotmynew'
-__news__ = '<blockquote>✨Обновление 0.0.6</blockquote>\n<blockquote>💡Что нового?</blockquote>\n<blockquote> - 🛠Исправлен плагин StartedPack, он не работал на Android устройствах из за одной библиотеки</blockquote>'
+__news__ = '<blockquote>✨Обновление 0.0.7b2</blockquote>\n<blockquote>💡Что нового?</blockquote>\n<blockquote> - 🛠Исправлен плагин StartedPack, он не работал на Android устройствах из за одной библиотеки</blockquote>'
 __python__ = '>=3.8'
 __modules__ = ['pyrogram', 'terminaltexteffects', 'requests', 'platform', 'pyfiglet']
